980083569f66d5e27749301ceb5353ed696f1609
José Luis López <joselopez@anzen.com.mx>
2022-01-27 21:20:51 -0600

    Feat: add functionality for CCBDataPersistence

--- Columbus Contact Book/CCBManager/CCBDataPersistence/CCBDataPersistence.swift
+++ Columbus Contact Book/CCBManager/CCBDataPersistence/CCBDataPersistence.swift
@@ -6,3 +6,132 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+public struct CCBDataPersistence {
+    
+    private let client: CCBManager
+    private var managedContext: NSManagedObjectContext
+    private var entity: NSEntityDescription
+        
+    public init(client: CCBManager){
+        self.client = client
+    
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        managedContext = appDelegate!.persistentContainer.viewContext
+        entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+    }
+        
+    public func addContact(data: Dictionary<String, String>){
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        person.setValue(data["name"], forKey: "name")
+        person.setValue(data["email"], forKey: "email")
+        person.setValue(data["phone"], forKey: "phone")
+        person.setValue(data["notes"], forKey: "notes")
+        person.setValue(data["address"], forKey: "address")
+
+        var people: [NSManagedObject] = []
+        
+        do {
+          try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+          print("Could not save. \(error), \(error.userInfo)")
+        }
+        
+    }
+    
+    public func getContacts() -> [NSManagedObject] {
+     
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        var people: [NSManagedObject] = []
+
+        do {
+          people = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+          print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        return people
+    }
+    
+    public func getContact(email: String) -> NSManagedObject {
+        
+        var person: NSManagedObject = NSManagedObject()
+
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Person")
+        fetchRequest.predicate = NSPredicate(format: "email = '\(NSString(string: email))'")
+        
+        do {
+            let result = try managedContext.fetch(fetchRequest)
+            person = result[0] as! NSManagedObject
+            
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        return person
+    }
+    
+    public func updateContact(email: String, data: Dictionary<String, String>) -> NSManagedObject {
+        
+        var person: NSManagedObject = NSManagedObject()
+        
+        print("email a actualizar: \(email)")
+        
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Person")
+        fetchRequest.predicate = NSPredicate(format: "email = %@", email)
+        
+        do{
+        
+            let result = try managedContext.fetch(fetchRequest)
+            person = result[0] as! NSManagedObject
+            
+            person.setValue(data["name"], forKey: "name")
+            person.setValue(data["email"], forKey: "email")
+            person.setValue(data["phone"], forKey: "phone")
+            person.setValue(data["address"], forKey: "address")
+            person.setValue(data["notes"], forKey: "notes")
+
+            do{
+                try managedContext.save()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+        }catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+        
+        return person
+    }
+    
+    public func deleteContact(email: String){
+        var person: NSManagedObject = NSManagedObject()
+        
+        print("email a eliminar: \(email)")
+        
+        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Person")
+        fetchRequest.predicate = NSPredicate(format: "email = %@", email)
+        
+        do{
+            let result = try managedContext.fetch(fetchRequest)
+            person = result[0] as! NSManagedObject
+
+            managedContext.delete(person)
+            
+            do {
+                try managedContext.save()
+            } catch let error as NSError {
+                print("Could not save. \(error), \(error.userInfo)")
+            }
+
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+}
+
--- Columbus Contact Book/CCBManager/CCBManager.swift
+++ Columbus Contact Book/CCBManager/CCBManager.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+public struct CCBManager{
+    
+    public init(){}
+    
+    public func CCBDataPersistence() -> CCBDataPersistence {
+        let ccbDataPersistence = Columbus_Contact_Book.CCBDataPersistence(client: self)
+        return ccbDataPersistence
+    }
+    
+}

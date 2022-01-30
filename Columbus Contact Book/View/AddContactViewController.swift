230a81ad52d3536275d0fda83b5e3c8f1d289166
José Luis López <joselopez@anzen.com.mx>
2022-01-30 16:43:38 -0600

    Feat: complete UI for add contact and add references to controller

--- Columbus Contact Book/View/AddContactViewController.swift
+++ Columbus Contact Book/View/AddContactViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class AddContactViewController: UIViewController {
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBOutlet weak var notesTesxtView: UITextView!
+    
+    @IBOutlet weak var helperLabel: UILabel!
+
+    @IBOutlet weak var addButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        customNotesTextView()
+        
     }
     
-    @objc func addTapped(){
+    func customNotesTextView(){
+        notesTesxtView.layer.borderColor = UIColor.systemGray6.cgColor;
+        notesTesxtView.layer.borderWidth = 1.0;
+        notesTesxtView.layer.cornerRadius = 5.0;
+    }
+    
+    @IBAction func addEvent(_ sender: Any) {
         
     }
 }

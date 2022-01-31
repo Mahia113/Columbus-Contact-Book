cb9fd4cd10def11884fdcd28871b4f5029126c5b
José Luis López <joselopez@anzen.com.mx>
2022-01-30 19:00:52 -0600

    Feat: complete UI/UX for add contact

--- Columbus Contact Book/View/AddContactViewController.swift
+++ Columbus Contact Book/View/AddContactViewController.swift
@@ -13,12 +13,10 @@ class AddContactViewController: UIViewController {
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
-    
     @IBOutlet weak var notesTesxtView: UITextView!
-    
     @IBOutlet weak var helperLabel: UILabel!
-
     @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,5 +33,38 @@ class AddContactViewController: UIViewController {
     
     @IBAction func addEvent(_ sender: Any) {
         
+        if validatorTextFiled() {
+            print("campos obligatorios llenos")
+        } else{
+            print("campos obligatorios no llenos")
+        }
+        
+    }
+    
+    
+    @IBAction func backEvent(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func validatorTextFiled() -> Bool {
+        
+        helperLabel.text = ""
+        
+        let name = nameTextField.text!
+        let email = emailTextField.text!
+        let phone = phoneTextField.text!
+        
+        if name.isEmpty {
+            helperLabel.text = "El campo nombre es obligatorio"
+            return false
+        } else if phone.isEmpty {
+            helperLabel.text = "El campo télefono es obligatorio"
+            return false
+        } else if email.isEmpty {
+            helperLabel.text = "El campo email es obligatorio"
+            return false
+        }
+        
+        return true
     }
 }

c3646d16b7f96cc22aa4cf7367a9db66eeaf0d2c
José Luis López <joselopez@anzen.com.mx>
2022-02-01 13:50:57 -0600

    Feat: add Weather feature

--- Columbus Contact Book/CCBManager/Weather/WeatherManager.swift
+++ Columbus Contact Book/CCBManager/Weather/WeatherManager.swift
@@ -2,7 +2,38 @@
 //  WeatherManager.swift
 //  Columbus Contact Book
 //
-//  Created by Anzen on 01/02/22.
+//  Created by Mahia113
 //
 
 import Foundation
+import Combine
+import UIKit
+
+public struct WeatherManager {
+    
+    private let client: CCBManager
+    private let URL = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0"
+    private let networkHandler: NetworkHandler = NetworkHandler()
+    
+    public init(client: CCBManager){
+        self.client = client
+    }
+    
+    func getWeatherData()  -> Future <WeatherModel, Error> {
+        return Future() { promise in
+            
+            networkHandler.performAPIRequestByURL(url: URL) {
+                switch $0 {
+                    case .success(let data):
+                        if let WeatherData: WeatherModel = self.networkHandler.decodeJSONData(data: data){
+                            promise(.success(WeatherData))
+                        }
+                    case .failure(let error):
+                        promise(.failure(error))
+                }
+            }
+            
+        }
+    }
+    
+}
--- Columbus Contact Book/CCBManager/Weather/WeatherModels.swift
+++ Columbus Contact Book/CCBManager/Weather/WeatherModels.swift
@@ -2,7 +2,26 @@
 //  WeatherModels.swift
 //  Columbus Contact Book
 //
-//  Created by Anzen on 01/02/22.
+//  Created by Mahia113
 //
 
 import Foundation
+
+
+public struct WeatherModel: Codable {
+    let product: String
+    let `init`: String
+    let dataseries: [DataseriesModel]
+}
+
+public struct DataseriesModel: Codable {
+    public let temp2m: Int
+    public let transparency: Int
+    public let lifted_index: Int
+    public let wind10m: Wind10msModel
+}
+
+public struct Wind10msModel: Codable {
+    public let direction: String
+    public let speed: Int
+}

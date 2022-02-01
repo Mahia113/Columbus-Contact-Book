09951a147c838e9e0629a075118b5ffbb2ead7ca
José Luis López <joselopez@anzen.com.mx>
2022-02-01 12:39:10 -0600

    Feat: add network handler for api requests

---
+++ Columbus Contact Book/CCBManager/Network/NetworkModels.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkModels.swift
+//  Columbus Contact Book
+//
+//  Created by Mahia113
+//
+
+import Foundation
+
+/**
+ Types of network errors
+ ### Properties
+ - **invalidURL**: URL request error.
+ - **invalidResponse**: HTTP request error.
+ - **apiError**: API request error.
+ - **decodingError**: Decoding request error.
+ */
+public enum NetworkHandlerError: Error {
+    case invalidURL
+    case invalidResponse(error: ErrorMessage)
+    case apiError
+    case decodingError
+}
+
+public struct ErrorMessage: Codable {
+    let error: String
+}

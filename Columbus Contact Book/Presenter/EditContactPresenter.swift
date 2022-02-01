aef9ddcfebb751c35bfeef8c8471494d49d448eb
José Luis López <joselopez@anzen.com.mx>
2022-02-01 09:02:09 -0600

    Feat: complete UX and logic for Edit contact

--- Columbus Contact Book/Presenter/EditContactPresenter.swift
+++ Columbus Contact Book/Presenter/EditContactPresenter.swift
@@ -2,7 +2,51 @@
 //  EditContactPresenter.swift
 //  Columbus Contact Book
 //
-//  Created by Anzen on 31/01/22.
+//  Created by Mahia113
 //
 
-import Foundation
+import UIKit
+
+protocol EditContactViewControllerDelegate: NSObjectProtocol {
+    func contactUpdated(update: Bool)
+}
+
+class EditContactPresenter {
+    
+    weak private var editContactViewControllerDelegate: EditContactViewControllerDelegate?
+    private let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    private let ccbManager: CCBManager = CCBManager()
+    
+    init() {}
+    
+    func setViewDelegate(editContactViewControllerDelegate: EditContactViewControllerDelegate?){
+        self.editContactViewControllerDelegate = editContactViewControllerDelegate
+    }
+    
+    func dismissController(controller: UIViewController){
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func updateContact(email: String, contact: ContactModel){
+        let update = ccbManager.CCBDataPersistence().updateContact(email: email, data: contact)
+        editContactViewControllerDelegate?.contactUpdated(update: update)
+    }
+    
+    func goToHome(controller: UIViewController){
+        print("goToHome")
+        
+        let temController = controller.presentingViewController
+        
+        controller.dismiss(animated: true, completion: {
+            
+            let homeViewController = self.storyBoard.instantiateViewController(withIdentifier: "home") as! HomeViewController
+
+            temController?.present(homeViewController, animated: true, completion: nil)
+        })
+        
+        
+        
+
+    }
+    
+}
--- Columbus Contact Book/View/EditContactViewController.swift
+++ Columbus Contact Book/View/EditContactViewController.swift
@@ -7,14 +7,63 @@
 
 import UIKit
 
-class EditContactViewController: UIViewController {
+class EditContactViewController: UIViewController, EditContactViewControllerDelegate {
+
+    @IBOutlet weak var viewContainer: UIView!
+    @IBOutlet weak var nameContact: UITextField!
+    @IBOutlet weak var phoneContact: UITextField!
+    @IBOutlet weak var emailContact: UITextField!
+    @IBOutlet weak var addressContact: UITextField!
+    @IBOutlet weak var noteContact: UITextView!
     
     var contact: ContactModel?
+    private var oldEmail: String?
+    
+    let editContactPresenter: EditContactPresenter = EditContactPresenter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        oldEmail = contact?.email
+        customViewContainer()
+        setValues()
+        editContactPresenter.setViewDelegate(editContactViewControllerDelegate: self)
+    }
+    
+    @IBAction func backEvent(_ sender: Any) {
+        editContactPresenter.dismissController(controller: self)
+    }
+    
+    @IBAction func updateEvent(_ sender: Any) {
+        let name = nameContact.text
+        let phone = phoneContact.text
+        let email = emailContact.text
+        let address = addressContact.text
+        let notes = noteContact.text
         
-        print("contact \(contact)")
+        let contactUpdate = ContactModel(name: name!, phone: phone!, email: email!, address: address!, notes: notes!)
+        
+        editContactPresenter.updateContact(email: oldEmail!, contact: contactUpdate)
+    }
+    
+    func contactUpdated(update: Bool) {
+        editContactPresenter.goToHome(controller: self)
+    }
+    
+    func setValues(){
+        nameContact.text = contact?.name
+        phoneContact.text = contact?.phone
+        emailContact.text = contact?.email
+        addressContact.text = contact?.address
+        noteContact.text = contact?.notes
+    }
+    
+    func customViewContainer(){
+        viewContainer.layer.backgroundColor = UIColor.white.cgColor
+        viewContainer.layer.cornerRadius = 20
+        viewContainer.layer.shadowColor = UIColor.black.cgColor
+        viewContainer.layer.shadowOffset = CGSize(width: 3, height: 3)
+        viewContainer.layer.shadowOpacity = 0.7
+        viewContainer.layer.shadowRadius = 4.0
     }
     
 }

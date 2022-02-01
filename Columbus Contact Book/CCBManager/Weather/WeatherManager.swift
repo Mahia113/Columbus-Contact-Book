67fbd97ecd1f46cc5e75b2a9b83390ae4282e79b
José Luis López <joselopez@anzen.com.mx>
2022-02-01 12:41:28 -0600

    Feat: add package and files for Weather feature

---
+++ Columbus Contact Book/CCBManager/Weather/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  Columbus Contact Book
+//
+//  Created by Anzen on 01/02/22.
+//
+
+import Foundation
---
+++ Columbus Contact Book/CCBManager/Weather/WeatherModels.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModels.swift
+//  Columbus Contact Book
+//
+//  Created by Anzen on 01/02/22.
+//
+
+import Foundation
